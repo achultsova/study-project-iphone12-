3c1ec3756802fc52e27a3179c0b903f468c456f2
Anastasiya <nchulczova@gmail.com>
2021-09-16 00:50:08 +0300

    feat: add description

--- js/script.js
+++ js/script.js
@@ -47,5 +47,62 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     };
+
+    const accordion = () => {
+        const characteristicsListElem = document.querySelector('.characteristics__list'),
+        characteristicsItemElems = document.querySelectorAll('.characteristics__item');
+    
+
+    characteristicsItemElems.forEach(elem => {
+        if (elem.children[1].classList.contains('active')) {
+            elem.children[1].style.height = elem.children[1].scrollHeight + 'px';
+        }
+    });
+
+    const open = (button, dropDown) => {
+        closeAllDrops();
+        dropDown.style.height = dropDown.scrollHeight + 'px';
+        button.classList.add('active');
+        dropDown.classList.add('active');
+    };
+
+    const close = (button, dropDown) => {
+        button.classList.remove('active');
+        dropDown.classList.remove('active');
+        dropDown.style.height = '';
+    };
+
+    const closeAllDrops = (button, dropDown) => {
+        characteristicsItemElems.forEach(elem => {
+            if (elem.children[0] !== button && elem.children[1] !== dropDown){
+                close(elem.children[0], elem.children[1]);
+            }
+        });
+    };
+
+    characteristicsListElem.addEventListener('click', event => {
+        const target = event.target;
+        if (target.classList.contains('characteristics__title')) {
+            const parent = target.closest('.characteristics__item');
+            const description = parent.querySelector('.characteristics__description');
+            if (description.classList.contains('active')) {
+            close (target, description);
+        } else {
+             open (target, description);
+        }
+        }
+    });
+
+    document.body.addEventListener('click', event => {
+        const target = event.target;
+        if (!target.closest('.characteristics__list')) {
+            closeAllDrops();
+        }
+    });
+};
+
+
     tabs();
+    accordion();
+    
 });
