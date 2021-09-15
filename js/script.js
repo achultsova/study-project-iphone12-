857bf4865830c2513adcb690ff43302411987b45
Anastasiya <nchulczova@gmail.com>
2021-09-15 20:08:48 +0300

    feat: another option for adding tabs

--- js/script.js
+++ js/script.js
@@ -4,28 +4,48 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const tabs = () => {
         const cardDetailChange = document.querySelectorAll('.card-detail__change'),
-        cardDetailsTitle = document.querySelectorAll('.card-details__title'),
-        cardImage = document.querySelectorAll('.card__image');
+        cardDetailsTitle = document.querySelector('.card-details__title'),
+        cardImageItem = document.querySelector('.card__image_item'),
+        cardDetailsPrice = document.querySelector('.card-details__price'),
+        descriptionMemory = document.querySelector('.description__memory'),
+        data = [
+            {
+                name: 'Смартфон Apple iPhone 12 Pro 128GB Graphite',
+                img: 'img/iPhone-graphite.png',
+                price: 97930,
+                memory: 128
+            },
+            {
+                name: 'Смартфон Apple iPhone 12 Pro 256GB Silver',
+                img: 'img/iPhone-silver.png',
+                price: 103780,
+                memory: 256
+            },
+            {
+                name: 'Смартфон Apple iPhone 12 Pro 128GB Pacific Blue',
+                img: 'img/iPhone-blue.png',
+                price: 102340,
+                memory: 128
+            },
+        ];
 
-        const hideAll = () => {
-            for (let i = 0; i < cardDetailChange.length; i++) {
-              cardDetailChange[i].classList.remove('active');
-              cardDetailsTitle[i].classList.remove('active');
-              cardImage[i].classList.remove('active');  
-            }
-            
+        const deactive = () => {
+            cardDetailChange.forEach(btn => btn.classList.remove('active'));
         };
 
-        for (let i= 0; i < cardDetailChange.length; i++) {
-
-            cardDetailChange[i].addEventListener('click', () => {
-                hideAll();
-                cardDetailChange[i].classList.add ('active');
-                cardDetailsTitle[i].classList.add ('active');
-                cardImage[i].classList.add ('active');
+        cardDetailChange.forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                if (!btn.classList.contains('active')) {
+                    deactive();
+                    btn.classList.add('active');
+                    cardDetailsTitle.textContent = data[i].name;
+                    cardImageItem.src = data[i].img;
+                    cardImageItem.alt = data[i].name;
+                    cardDetailsPrice.textContent = data[i].price + '₽';
+                    descriptionMemory.textContent = `Встроенная память (ROM) ${data[i].memory} ГБ`;  
+                }
             });
-        }
+        });
     };
-
     tabs();
 });
